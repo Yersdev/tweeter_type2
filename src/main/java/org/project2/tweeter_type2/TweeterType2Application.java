ec99b010a6ab2fb87f2c9771064acd2acf19d69f
Yessimov Daniyar <esimov451@gmail.com>
2024-10-24 15:43:35 +0500

    Initial commit

---
+++ src/main/java/org/project2/tweeter_type2/TweeterType2Application.java
@@ -0,0 +1,13 @@
+package org.project2.tweeter_type2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweeterType2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweeterType2Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/project2/tweeter_type2/TweeterType2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project2.tweeter_type2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweeterType2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
