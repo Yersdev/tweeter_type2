b7dc0df58b0ec42fb20f39ec87a8b77b71db9949
Amangeldin Yersultan <amangeldinyersultandev@gmail.com>
2024-10-25 17:36:21 +0500

    validation tweet page

--- src/main/java/org/project2/tweeter_type2/controller/MainController.java
+++ src/main/java/org/project2/tweeter_type2/controller/MainController.java
@@ -5,41 +5,52 @@ import org.project2.tweeter_type2.model.Message;
 import org.project2.tweeter_type2.services.MessageServices;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class MainController {
 
-     private final MessageServices messageServices;
-     @GetMapping("/")
-     public String greeting() {
-         return "greeting";
-     }
+    private final MessageServices messageServices;
+
+    @GetMapping("/")
+    public String greeting() {
+        return "greeting";
+    }
 
     @GetMapping("/main")
-    // Путь для главной страницы
     public String mainPage(Model model) {
-        List<Message> messages = messageServices.findAll(); // Не нужно кастовать, возвращает List<Message>
+        List<Message> messages = messageServices.findAll();
         model.addAttribute("messages", messages);
+        model.addAttribute("message", new Message());
         return "main";
     }
 
-    @PostMapping("/main") // Убедитесь, что это совпадает с действием формы
-    public String addMessage(@RequestParam String text, @RequestParam String tag, Model model) {
-        Message newMessage = new Message();
-        newMessage.setText(text);
-        newMessage.setTag(tag);
+    @PostMapping("/main")
+    public String addMessage(@Valid Message message, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            List<Message> messages = messageServices.findAll();
+            model.addAttribute("messages", messages);
+            return "main"; // Возвращаем ту же страницу с отображением ошибок
+        }
 
-        // Сохранение нового сообщения в репозитории
-        messageServices.save(newMessage);
-
-        // Получение обновленного списка сообщений
-        List<Message> messages = messageServices.findAll(); // Не нужно кастовать
-        model.addAttribute("messages", messages); // Добавляем список сообщений в модель
-
-        return "main"; // Возвращаем main.mustache
+        messageServices.save(message);
+        return "redirect:/main";
     }
+//    @PostMapping("/main")
+//    public String updateMessage(@Valid Message message, BindingResult bindingResult, Model model) {
+//        if (bindingResult.hasErrors()) {
+//            List<Message> messages = messageServices.findAll();
+//        }
+//        Message updMessage = new Message();
+//        updMessage.setId(message.getId());
+//        updMessage.setText(message.getText());
+//        updMessage.setTag(message.getTag());
+//        messageServices.save(updMessage);
+//        return "redirect:/main";
+//    }
 }
--- src/main/java/org/project2/tweeter_type2/model/Message.java
+++ src/main/java/org/project2/tweeter_type2/model/Message.java
@@ -1,7 +1,12 @@
 package org.project2.tweeter_type2.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+import lombok.NonNull;
 
 @Entity
 @Table(name="message")
@@ -11,9 +16,12 @@ public class Message {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
+    @PositiveOrZero(message = "ID должен быть неотрицательным числом")
     private int id;
+    @Size(min = 10, max = 250, message = "Текст должен содержать от 10 до 250 символов")
     @Column(name = "text")
     private String text;
+    @Size(min = 3, max = 50, message = "Тэг должен содержать от 3  до 50 символов")
     @Column(name = "tag")
     private String tag;
 
